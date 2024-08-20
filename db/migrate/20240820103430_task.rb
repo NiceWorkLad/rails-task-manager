b3d310d55d6c5fbf4b160d1a1cdce0ffc392c22a
louis whittleadams <louiswhittle7d1@gmail.com>
2024-08-20 11:45:10 +0100

    added values on generation instead of migration update

--- db/migrate/20240820103430_task.rb
+++
@@ -1,7 +0,0 @@
-class Task < ActiveRecord::Migration[7.1]
-  def change
-    add_column :tasks, :title, :string
-    add_column :tasks, :details, :text
-    add_column :tasks, :completed, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_20_103430) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_20_103400) do
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
-    t.text "details"
-    t.boolean "completed"
   end
 
 end
